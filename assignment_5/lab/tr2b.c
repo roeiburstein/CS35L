c3a7c21d3a12d6caf09099e0ad9823e4d1589a1d
roeiburstein <roei.burstein@gmail.com>
2019-11-03 17:39:39 -0800

    Begin assignment 5 and finish tr2b.c

---
+++ assignment_5/lab/tr2b.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, const char *argv[]) {
+    if (argc == 3) {
+        const char * from = argv[1], * to = argv[2];
+        size_t fromLen = strlen(from);
+        size_t toLen = strlen(to);
+        short asciiArray[255] = {0};
+
+        if(fromLen != toLen) {
+            fprintf(stderr, "ERROR, first and second arguments are different lengths, exiting program");
+            exit(1);
+        }
+
+        for(size_t i = 0; i < fromLen; i++) {
+            if(asciiArray[from[i]] == 1) {
+                fprintf(stderr, "ERROR, repeating characters in first argument, exiting program");
+                exit(1);
+            }
+            asciiArray[(int)from[i]] = 1;
+        }
+
+        char c;
+        while(c != EOF){
+            c = getchar();
+            if(asciiArray[c] == 1){
+                size_t charIndex = -1;
+                for(size_t j = 0; j < fromLen; j++) {
+                    if(from[j] ==  c) {
+                        charIndex = j;
+                    }
+                }
+                putchar(to[charIndex]);
+            }
+            else{
+                putchar(c);
+            }
+        }
+    }
+
+
+
+
+
+    //abc xyz
+    //abccba
+}
